fef5c92b283e691feb6af07beb953a1c02c2e982
Tom Macmichael <tommacmichael@Toms-MacBook-Pro.local>
2015-12-24 18:48:22 +0000

    Basic setup of homepage working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,120 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+    <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8"/>
+    <meta name="keywords" content="developer, tom macmichael, student, edinburgh, php, html, maths"/>
+    <meta name="copyright" content="Copyright Maccery.com 2013">
+    <meta name="author" content="Maccery">
+    <meta name="email" content="tom@yourtaximeter.com">
+    <meta name="Rating" content="General">
+    <meta name="Revisit-after" content="1 Days">
+    <meta name="ROBOTS" content="INDEX, FOLLOW"/>
+    <meta property="description" content="Freelance web developer from The University of Edinburgh."/>
+    <title>Maccery - Web Development</title>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}" type="text/css"/>
+    <link rel="stylesheet" href="{{ asset('css/framework.css') }}" type="text/css"/>
+    <link rel="stylesheet" href="{{ asset('css/global.css') }}" type="text/css"/>
+    <meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1; user-scalable=0;">
+    <script type="text/javascript" src="http://use.typekit.net/isg3hdh.js"></script>
+    <script type="text/javascript">try {
+            Typekit.load();
+        } catch (e) {
+        }</script>
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+<body>
+<div id="cover">
+    <div id="topbanner">
+        <ul>
+            <li><b>maccery web development</b></li>
+            <li><a target="_blank" href="mailto:tom@yourtaximeter.com">get in touch</a></li>
+        </ul>
+        <p>maccery</p>
+    </div>
+</div>
+<div class="container">
+    <h1>Portfolio</h1>
+    <div class="row">
+        <div class="col-sm-4">
+            <h2>YourTaximeter</h2>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+            <p>The most popular and most accurate taxi fare calculator in GB, attracting over 45,000 punters a
+                month.</p>
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+            <p>(2010) <a href="http://yourtaximeter.com" target="_blank">YourTaximeter.com</a></p>
         </div>
-    </body>
+        <div class="col-sm-4">
+            <h2>/ps</h2>
+
+            <p>Analyse your personal statement, simulate UCAS's strange line counting and much more. With over 12,000
+                monthly users during winter!</p>
+
+            <p>(2010) <a href="/ps" target="_blank">/ps</a></p>
+        </div>
+        <div class="col-sm-4">
+            <h2>/maths</h2>
+
+            <p>The statistics website with (the first ever) online Spearman's Rank calculator alongside a
+                Newton-Raphson calculator and more. Designed for GCSE and A Level students.</p>
+
+            <p>(2009) <a href="/maths" target="_blank">/maths</a></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <h2>/ps</h2>
+
+            <p>Analyse your personal statement, simulate UCAS's strange line counting and much more - all in real
+                time. With over 12,000 monthly users during winter!</p>
+
+            <p>(2010) <a href="/ps" target="_blank">/ps</a></p>
+        </div>
+        <div class="col-sm-4">
+            <h2>SnaxCafe</h2>
+
+            <p>A sleek design for a cafe (and breakfast delivery service) based in central Edinburgh.</p>
+
+            <p>(2013) <a href="http://snaxcafe.com" target="_blank">SnaxCafe.com</a></p>
+        </div>
+        <div class="col-sm-4">
+            <h2>Bedrunken</h2>
+
+            <p>A 24 hour project, whilst at University, aimed at optimising the purchase of alcohol drinks - for
+                students.</p>
+
+            <p>(2013) <a href="http://bedrunken.com" target="_blank">Bedrunken.com</a></p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <h2>ECFS</h2>
+
+            <p>ECFS has raised over a quarter of a million pounds for charities throughout the United Kingdom. A simple,
+                stylish design with Facebook and Wordpress integration.</p>
+
+            <p>(2014) <a href="http://edinburghcharityfashionshow.org" target="_blank">EdinburghCharityFashionShow.org</a></p>
+        </div>
+        <div class="col-sm-4">
+            <h2>/anagram</h2>
+
+            <p>A simple real-time anagram solver with easy-to-use interface.</p>
+
+            <p>(2010) <a href="/anagram" target="_blank">/anagram</a></p>
+        </div>
+        <div class="col-sm-4">
+            <h2>Festifinder</h2>
+
+            <p>The first ever European music festival search engine, with a gorgeous design and interface.</p>
+
+            <p>(2013-2014. Archived)</p>
+        </div>
+    </div>
+</div>
+<hr>
+<div id="copyright" class="container">
+    <p>Copyright &copy; 2013 <a href="http://maccery.com" target="_blank">Maccery</a></p>
+</div>
+</body>
 </html>
